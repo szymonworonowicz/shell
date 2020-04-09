860c03916018e21f73d6f5d57ba621da03d3c711
szymonworonowicz <szymonworonowicz25@gmail.com>
2020-04-09 20:14:44 +0200

    pipe cos wadzi

--- main.c
+++ main.c
@@ -67,13 +67,14 @@ static void pipeline(char ***cmd)
         }
         else if (pid == 0)
         {
+            umask(0);
             dup2(fdd, 0);
             if (*(cmd + 1) != NULL)
             {
                 dup2(fd[1], 1);
             }
-            printf("%s %s \n", (*cmd)[0], *cmd);
             close(fd[0]);
+            //execvp((*cmd)[0],(*cmd)[1]);
             execvp((*cmd)[0], *cmd);
             exit(1);
         }
@@ -94,46 +95,52 @@ int main(int argc, char *argv[])
     char *task;
     int countoftasks;
     char *pipes[100];
-    char *ls[] = {"ls", "-al", NULL};
     //https://gist.github.com/iomonad/a66f6e9cfb935dc12c0244c1e48db5c8
-    // char *rev[] = {"rev", NULL};
-    // char *nl[] = {"nl", NULL};
-    // char *cat[] = {"cat", "-e", NULL};
-    char **cmd[] = {ls, NULL};
+    char *ls[] = {"ls", "-al", NULL};
+    char *rev[] = {"rev", NULL};
+    char *nl[] = {"nl", NULL};
+    char *cat[] = {"cat", "-e", NULL};
+    char **cmd[] = {ls, rev, nl, cat, NULL};
+
     pipeline(cmd);
+
     while ((task = fgets(bufor, sizeof(bufor), stdin)) != 0)
     {
         int redirect = splittoTask(task, &countoftasks, &pipes);
         int i = 0;
-        char **cmda[countoftasks+1];
+        char **cmda[countoftasks + 1];
         for (i = 0; i < countoftasks; i++)
         {
             if (i == countoftasks - 1)
             {
-                int len = sizeof(pipes[i]) / sizeof(char);
-                printf("%d", len);
-                strncpy(pipes[i], pipes[i], len - 5);
-                pipes[i][len - 3] = '\0';
+                int j, enter = 0;
+                for (j = 0; pipes[i][j] != '\0'; j++)
+                {
+                    if (pipes[i][j] == '\n')
+                    {
+                        enter = 1;
+                        break;
+                    }
+                }
+                if (enter == 1)
+                {
+                    pipes[i][j] = '\0';
+                }
             }
             char *command = strtok(pipes[i], " ");
             char *args = strtok(NULL, " ");
             if (args == NULL)
             {
-                //printf("%d \n", i);
                 char *comm[] = {command, args};
-                //printf("%s %s\n", comm[0], comm[1]);
-                cmda[i] = comm;
+                *cmda[i] = *comm;
             }
             else
             {
-                //printf("%s %s\n", command, args);
                 char *comm[] = {command, args, NULL};
-                cmda[i] = comm;
-                //cmd[i] = &comm;
+                *cmda[i] = *comm;
             }
         }
         cmda[i] = NULL;
         pipeline(cmda);
-        
     }
 }
